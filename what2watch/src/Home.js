9258ab83d7e2ec20aaead3c21a0d370b71fed846
Chinonso Okafor <chinonso@Chinonsos-MacBook-Pro.local>
2022-11-29 22:13:59 -0500

    My work on the Login and Registration Pages

---
+++ what2watch/src/Home.js
@@ -0,0 +1,11 @@
+
+function Home() {
+    console.log("Home");
+    return (
+        <div>
+            This is Home. It is not my problem.
+        </div>
+    );
+}
+
+export default Home;
---
+++ what2watch/src/Login.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { onAuthStateChanged, signOut, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./Firebase";
+
+
+function Login() {
+
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
+    const logIn = async () =>  {
+        try {
+            const user = await signInWithEmailAndPassword(
+              auth,
+              loginEmail,
+              loginPassword
+            );
+            console.log(user);
+          } catch (error) {
+            console.log(error.message);
+          }
+    };
+
+    const logOut = async () =>  {
+        await signOut(auth);
+    };
+
+    const [user, setUser] = useState({});
+
+    React.useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => setUser(currentUser))
+      }, []) 
+
+
+    return (
+        <div>
+            <div>
+                <h1>Login</h1>
+
+                <label ><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" onChange={(event) => {setLoginEmail(event.target.value);}} name="email" required/><br></br>
+
+                <label ><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" onChange={(event) => {setLoginPassword(event.target.value);}} name="psw" required/><br></br>
+
+                <button onClick={logIn} >Login</button>
+            </div>
+            <div>
+                <h4> User Logged In: </h4>
+                {user?.email}
+                <button onClick={logOut}>Log out</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
