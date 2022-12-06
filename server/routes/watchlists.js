9039888361d39c9bdc122a60a820b48a85439ea6
Chinonso Okafor <chinonso@Chinonsos-MacBook-Pro.local>
2022-12-06 13:42:26 -0500

    Added api endpoints for managing our watchlist

--- server/routes/watchlists.js
+++ server/routes/watchlists.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getAllWatchlistsForUser } = require('../services/watchlist');
+const { getAllWatchlistsForUser, createWatchlist, addMovieToWatchlist, removeMovieFromWatchlist, deleteWatchlist } = require('../services/watchlist');
 
 const router = express.Router();
 
@@ -13,5 +13,63 @@ router.get('/', async (req, res) => {
     return res.status(500).json({ message: e.toString() });
   }
 });
+
+// route to add to watchlist
+router.post('/', async (req, res) => {
+  try {
+    const userId = '638bec9c887eade40b5a68d4';
+    const watchlistId = '222jh2';
+    const movieId = '22222';
+    const movieTitle = '2jkwb2';
+    const moviePoster = "jsddfdsf";
+    const watchlists = await addMovieToWatchlist(watchlistId, userId, movieId,
+       movieTitle, moviePoster);
+    return res.status(200).json({ watchlists });
+  } catch (e) {
+    return res.status(500).json({ message: e.toString() });
+  }
+});
+
+
+// route to delete from watchlist
+router.delete('/', async (req, res) => {
+  try {
+    const userId = '638bec9c887eade40b5a68d4';
+    const watchlistId = '222jh2';
+    const movieId = '22222';
+    const watchlists = await removeMovieFromWatchlist(watchlistId, userId,
+       movieId);
+    return res.status(200).json({ watchlists });
+  } catch (e) {
+    return res.status(500).json({ message: e.toString() });
+  }
+});
+
+
+// route to create watchlist
+router.put('/', async (req, res) => {
+  try {
+    const userId = '638bec9c887eade40b5a68d4';
+    const title = '222jh2';
+    const watchlists = await createWatchlist(title, userId);
+    return res.status(200).json({ watchlists });
+  } catch (e) {
+    return res.status(500).json({ message: e.toString() });
+  }
+});
+
+
+// route to delete watchlist
+router.delete('/', async (req, res) => {
+  try {
+    const userId = '638bec9c887eade40b5a68d4';
+    const watchlistId = '222jh2';
+    const watchlists = await deleteWatchlist(watchlistId, userId);
+    return res.status(200).json({ watchlists });
+  } catch (e) {
+    return res.status(500).json({ message: e.toString() });
+  }
+});
+
 
 module.exports = router;
