2323f6821ed52ae9f633f2c472622dc38bf43b86
Surakshya Aryal <suhhraxya@gmail.com>
2022-12-06 22:13:52 -0800

    fix eslint error

--- client/src/Banner.js
+++ client/src/Banner.js
@@ -1,32 +1,28 @@
 import React from 'react';
-import './Banner.css'
+import './Banner.css';
 
 function Banner() {
-    return (
-        <header
-          className="banner"
-          style={{
-            backgroundSize: "cover",
-            backgroundImage: `url("https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-04.jpg")`,
-            backdropPosition: "center center",
-          }}
-        >
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url("https://collider.com/wp-content/uploads/the-avengers-movie-poster-banners-04.jpg")`,
+        backdropPosition: 'center center',
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Avengers</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Recommendations</button>
+          <button className="banner__button">My List </button>
+        </div>
 
-          <div className="banner__contents">
-            <h1 className="banner__title">
-              Avengers
-            </h1>
-            <div className="banner__buttons">
-              <button className="banner__button">Recommendations</button>
-              <button className="banner__button">My List </button>
-            </div>
+        <div className="banner__description"></div>
+      </div>
+      <div className="banner__fadeBottom" />
+    </header>
+  );
+}
 
-            <h1 className="banner__description"></h1>
-          </div>
-          <div className="banner__fadeBottom" />
-        </header>
-      );
-    }
-    
-
-export default Banner
+export default Banner;
