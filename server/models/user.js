e2df543e0cfd6b315afae9b9f230b99c5b6b8b42
surakshya00 <63863346+surakshya00@users.noreply.github.com>
2022-12-12 15:00:42 -0800

    Show movie details (#8)
    * update home page details
    
    * add route to get providers
    
    * show providers

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
+  firstName: { type: String, required: false },
+  lastName: { type: String, required: false },
   email: { type: String, required: true, unique: true },
 });
 
